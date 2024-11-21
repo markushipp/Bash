041dfde75e1cdd1a291ee3d124b0d7742739729a
markushipp <markus.hipp@mailbox.org>
2024-11-21 18:59:13 +0100

    test2

--- remove_stopped_docker_containers.sh
+++ remove_stopped_docker_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 stopped_containers=$(docker ps -a -q -f status=exited)
-#blah
+
 if [ -n "$stopped_containers" ]; then
   # Remove all stopped containers
   docker rm $stopped_containers
